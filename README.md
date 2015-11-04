9c498fe4946887a6589ba39fda488c1c747abf53
Oscar Ponce <wigahluk@gmail.com>
2015-11-03 22:10:01 -0800

    Webpack is now configured to use an Express server in order to be able to load node_modules that are not available in the regular webpack development server.

--- README.md
+++ README.md
@@ -3,7 +3,17 @@ Date Time Picker
 
 Another angular directive for selecting date and time ranges.
 
-Sample
-------
+Creating a new build is done running:
 
-Run `npm start` and go to `http://localhost:8000/example/`
+    npm run build
+
+
+Development
+-----------
+
+The sample page is configured to run using a node server and a webpack development server
+inside of it.
+
+    npm start
+
+and then go to `http://localhost:3000`
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
 /* global __dirname */
+var webpack = require('webpack');
 var path = require('path');
 var buildPath = path.resolve(__dirname, 'build');
 var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+var mainPath = path.resolve(__dirname, 'src', 'main.js');
+
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var config = {
-    entry: "./src/main.js",
+    devtool: 'eval',
+    entry: [
+        'webpack/hot/dev-server',
+        'webpack-dev-server/client?http://localhost:8080',
+        mainPath
+    ],
     output: {
         path: buildPath,
-        filename: 'date-time-picker.js'
+        filename: 'date-time-picker.js',
+        publicPath: '/build/'
     },
     externals: {
         "angular": "angular"
@@ -15,12 +25,27 @@ var config = {
     module: {
         loaders: [
             {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+            },
+            {
                 test: /\.js$/,
                 loader: 'babel',
                 exclude: [nodeModulesPath]
             }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin("[name].css"),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.ProvidePlugin({
+            'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+        })
+    ]
 };
 
 module.exports = config;
