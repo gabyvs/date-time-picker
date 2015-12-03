f2ffd773975146e516a5aecccf334d80d4275c92
Gabriela Vazquez <gabs.vz@gmail.com>
2015-12-03 15:24:46 -0800

    Removing hot loader plugin from distribution files. Only one webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,16 @@
 /* global __dirname */
-var webpack = require('webpack');
-var path = require('path');
-var buildPath = path.resolve(__dirname, 'build');
-var nodeModulesPath = path.resolve(__dirname, 'node_modules');
-var mainPath = path.resolve(__dirname, 'app', 'main.js');
-var cssPath = path.resolve(__dirname, 'app', 'dateTimePicker.less');
-
+var webpack = require("webpack");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-var config = {
-    devtool: 'eval',
-    entry: [
-        'webpack/hot/dev-server',
-        'webpack-dev-server/client?http://localhost:8080',
-        cssPath,
-        mainPath
-    ],
-    output: {
-        path: buildPath,
-        filename: 'dateTimePicker.js',
-        publicPath: '/build/'
-    },
-    externals: {
-        'angular': 'angular',
-        'jQuery': 'jQuery',
-        'moment': 'moment',
-        'lodash': '_'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-            },
-            {
-                test: /\.less$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel',
-                exclude: [nodeModulesPath]
-            },
-            {
-                test: /\.html$/,
-                loader: "html-loader",
-                exclude: [nodeModulesPath]
-            },
-            {
-                test: /\.(jpg|png)$/,
-                loader: 'url-loader?limit=100000'
-            },
-            {
-                test: /\.svg$/,
-                loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin("[name].css"),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.ProvidePlugin({
-            'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-        })
-    ]
-};
+module.exports = exports = Object.create(require("./webpack.base.config.js"));
 
-module.exports = config;
+exports.devtool = 'eval';
+exports.entry = ['webpack/hot/dev-server', 'webpack-dev-server/client?http://localhost:8080'].concat(exports.entry);
+exports.plugins = [
+    new ExtractTextPlugin("[name].css"),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+        'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+    })
+];
 
--- webpack.ng.config.js
+++
@@ -1,6 +0,0 @@
-var webpack = require("webpack");
-module.exports = exports = Object.create(require("./webpack.config.js"));
-
-exports.output.filename = "dateTimePicker.browser.js";
-exports.output.library = "dateTimePicker";
-exports.output.libraryTarget = "var";
